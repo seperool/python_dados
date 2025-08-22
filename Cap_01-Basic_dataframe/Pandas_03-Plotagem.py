63c191938e43e1b6c273eb09c7861de82a8b71fe
seperool <seperool@gmail.com>
2025-08-22 10:33:13 -0300

    código comentado

--- Cap_01-Basic_dataframe/Pandas_03-Plotagem.py
+++ Cap_01-Basic_dataframe/Pandas_03-Plotagem.py
@@ -1,19 +1,28 @@
 # --- SCRIPT: OBTENDO SUBCONJUNTOS DE DADOS EM DATAFRAMES PANDAS ---
 
-import pandas as pd # Importa a biblioteca pandas, atribuindo-lhe o alias 'pd'.
-import matplotlib.pyplot as plt # Importa a biblioteca matplotlib para visualização de dados.
+import pandas as pd # Importa a biblioteca pandas.
+import matplotlib.pyplot as plt # Importa a biblioteca matplotlib para gráficos.
 
 #------------------------------------------------------------------------------
 
 # --- Carregamento e Inspeção Inicial ---
 
 # Carrega o arquivo 'gapminder.tsv' em um DataFrame, usando tabulação como separador.
-# df = pd.read_csv('../Data/Cap_01/gapminder.tsv', sep='\t')
 df = pd.read_csv(r'C:\Users\Sergio\github\python_dados\Data\Cap_01\gapminder.tsv', sep='\t')
 
+# --- Análise e Visualização ---
+
+# Agrupa os dados por ano e calcula a média da expectativa de vida para cada ano.
 global_yearly_life_expectancy = df.groupby('year')['lifeExp'].mean()
+
+# Exibe a média da expectativa de vida por ano.
 print(global_yearly_life_expectancy)
 
+# Cria um gráfico de linha com os anos no eixo X e a expectativa de vida no eixo Y.
 plt.plot(global_yearly_life_expectancy.index, global_yearly_life_expectancy.values)
+
+# Define o rótulo do eixo X.
 plt.xlabel('Year')
+
+# Mostra o gráfico.
 plt.show()
